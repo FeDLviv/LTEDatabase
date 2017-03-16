a46567241a1a6b3ac8e5078b3ba102d8c25b2201
FeDLviv <fed.lviv@gmail.com>
2017-03-16 17:03:37 +0200

    Add sort (objects)

--- LTEDatabase/ViewModel/ObjectsViewModel.cs
+++ LTEDatabase/ViewModel/ObjectsViewModel.cs
@@ -12,6 +12,7 @@ namespace LTEDatabase.ViewModel
     class ObjectsViewModel : BaseViewModel
     {
         private string[] typeOfObjects = new string[] { "котельня", "ЦТП", "ІТП", "майстерня", "ТК", "склад", "гуртожиток" };
+        private static MyCompare compare = new MyCompare();
 
         private ObservableCollection<Region> regions;
         public ObservableCollection<Region> Regions 
@@ -51,10 +52,10 @@ namespace LTEDatabase.ViewModel
                 region.Types = new ObservableCollection<Type>(types);
                 foreach (Type type in region.Types)
                 {
-                    var list = from z in temp
-                               where z.region == region.Name && z.type == type.Name
-                               orderby z.address
-                               select z;
+                    var list = (from z in temp
+                                where z.region == region.Name && z.type == type.Name
+                                //orderby z.address
+                                select z).OrderBy((z => z), compare);
                     type.Objects = new ObservableCollection<objects>(list);
                 }
             }
@@ -70,6 +71,53 @@ namespace LTEDatabase.ViewModel
         {
             public string Name { get; set; }
             public ObservableCollection<Type> Types { get; set; }
+        }
+
+        public class MyCompare : IComparer<objects>
+        {
+            public int Compare(objects x, objects y)
+            {
+                int point1 = x.address.IndexOf(',');
+                int point2 = y.address.IndexOf(',');
+
+                if (point1 != -1 && point2 != -1)
+                {
+                    string street1 = x.address.Substring(0, point1);
+                    string street2 = y.address.Substring(0, point2);
+
+                    string other1 = x.address.Substring(point1 + 2, x.address.Length - (point1 + 2));
+                    string other2 = y.address.Substring(point2 + 2, y.address.Length - (point2 + 2));
+                    
+                    int number1;
+                    int number2;
+
+                    if (Int32.TryParse(other1, out number1) && Int32.TryParse(other2, out number2))
+                    {
+                        if (street1.CompareTo(street2) == 0)
+                        {
+                            if (number1 > number2)
+                            {
+                                return 1;
+                            }
+                            else if (number1 < number2)
+                            {
+                                return -1;
+                            }
+                        }
+                        return x.address.CompareTo(y.address);
+                    }
+                    else
+                    { 
+                        //ДОПИСАТИ
+                    }
+                    //ДОПИСАТИ
+                    return x.address.CompareTo(y.address);
+                }
+                else
+                {
+                    return x.address.CompareTo(y.address);
+                }
+            }
         }
     }
 }
