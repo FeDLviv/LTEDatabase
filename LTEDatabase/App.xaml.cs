ef594478a7432e24f1673afb1f2acb57402f6b67
FeDLviv <fed.lviv@gmail.com>
2017-03-22 16:32:52 +0200

    Catch main exception (MessageBox), some change WPF.txt, change animation DataGrid (MainWindow.xaml)

--- LTEDatabase/App.xaml.cs
+++ LTEDatabase/App.xaml.cs
@@ -1,8 +1,19 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 
 namespace LTEDatabase
 {
     public partial class App : Application
     {
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            Exception ex = e.Exception;
+            while (ex.InnerException != null)
+            {
+                ex = ex.InnerException;
+            }
+            MessageBox.Show(ex.Message,"Помилка", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.OK, MessageBoxOptions.DefaultDesktopOnly);
+            Environment.Exit(1);
+        }
     }
 }
