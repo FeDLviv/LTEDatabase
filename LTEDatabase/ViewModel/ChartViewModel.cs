88267a14703568a18d0a163560468826bf6d7960
s25-vp1 <s25-vp1@it-step.edu>
2017-03-30 20:45:55 +0300

    Change command in ChartViewModel (slider-filter), add tiles (meter, TC, contract)

--- LTEDatabase/ViewModel/ChartViewModel.cs
+++ LTEDatabase/ViewModel/ChartViewModel.cs
@@ -16,10 +16,17 @@ namespace LTEDatabase.ViewModel
     {
         private const int LIMIT = 8;
         private const string QUERY_MOTOR = "SELECT series AS Name, COUNT(*) AS Value FROM motors_lte GROUP BY Name ORDER BY Value DESC;";
+        private const string QUERY_METER = "SELECT meters.type AS Name, COUNT(meters_lte.idMLTE) AS Value FROM meters RIGHT JOIN meters_lte USING (idMeter) GROUP BY Name ORDER BY Value DESC;";
+        private const string QUERY_TC = "SELECT CONCAT_WS(' ', tc.type, tc.coefficient) AS Name, COUNT(tc_lte.idTCLTE) AS Value FROM tc RIGHT JOIN tc_lte USING (idTC) GROUP BY Name ORDER BY Value DESC;";
+        private const string QUERY_CONTRACT = "SELECT IFNULL(organization, 'без договору') AS Name, COUNT(*) AS Value FROM objects WHERE type NOT IN ('склад', 'ТЕЦ', 'ТЦ') GROUP BY Name ORDER BY organization IS NULL, Value DESC;";
         private const string TITLE_MOTOR = "Двигуни на об'єктах ЛМКП \"Львівтеплоенерго\"";
+        private const string TITLE_METER = "Лічильники на об'єктах ЛМКП \"Львівтеплоенерго\"";
+        private const string TITLE_TC = "Трансформатори струму на об'єктах ЛМКП \"Львівтеплоенерго\"";
+        private const string TITLE_CONTRACT = "Договори на електропостачання об'єктів ЛМКП \"Львівтеплоенерго\"";
 
         private string query;
         private List<ChartData> list = new List<ChartData>();
+        private int previousFilterValue;
         
         private bool showChart = false;
         public bool ShowChart 
@@ -120,22 +127,27 @@ namespace LTEDatabase.ViewModel
         }
         
         public ICommand PieChartMotorCommand { get; set; }
+        public ICommand PieChartMeterCommand { get; set; }
+        public ICommand PieChartTCCommand { get; set; }
+        public ICommand PieChartContractCommand { get; set; }
         public ICommand FilterCommand { get; set; }
         public ICommand ChartHiddenCommand { get; set; }
 
         public ChartViewModel()
         {
-            //ФІЛЬТРАЦІЯ, ЯКЩО ЗМІНЕНО  ЗНАЧЕННЯ В SLIDER (Thumb.DragCompleted)
-            
             //PROGRESSBAR
             
             //ЗНИКНЕННЯ ToolTip при розкритті/закритті діаграми
             //DATALABELS ПРОБЛЕМА СУМІСНОСТІ З TOOLTIP
             //EXCEPTION SQL
+            //ПРОКРУТКА КРУГОВОЇ ДІАГРАМИ
             
             //ГІСТОГРАММА (Basic Column)
             
             PieChartMotorCommand = new BaseCommand(DoPieChartMotorCommand);
+            PieChartMeterCommand = new BaseCommand(DoPieChartMeterCommand);
+            PieChartTCCommand = new BaseCommand(DoPieChartTCCommand);
+            PieChartContractCommand = new BaseCommand(DoPieChartContractCommand);
             FilterCommand = new BaseCommand(DoFilterCommand);
             ChartHiddenCommand = new BaseCommand(DoChartHiddenCommand);
         }
@@ -147,10 +159,63 @@ namespace LTEDatabase.ViewModel
             LoadData();
         }
 
+        private void DoPieChartMeterCommand(object obj)
+        {
+            TitleChart = TITLE_METER;
+            query = QUERY_METER;
+            LoadData();
+        }
+
+        private void DoPieChartTCCommand(object obj)
+        {
+            TitleChart = TITLE_TC;
+            query = QUERY_TC;
+            LoadData();
+        }
+
+        private void DoPieChartContractCommand(object obj)
+        {
+            TitleChart = TITLE_CONTRACT;
+            query = QUERY_CONTRACT;
+            LoadData();
+        }
+
+        private void DoFilterCommand(object obj)
+        {
+            if (FilterValue != previousFilterValue)
+            {
+                previousFilterValue = FilterValue;
+                Data.Clear();
+                int count = 0;
+                foreach (ChartData x in list)
+                {
+                    if (x.Value >= FilterValue)
+                    {
+                        Data.Add(new PieSeries() { Title = x.Name, Values = new ChartValues<int> { x.Value } });
+                    }
+                    else
+                    {
+                        count += x.Value;
+                    }
+                }
+                if (count > 0)
+                {
+                    Data.Add(new PieSeries() { Title = "решта", Values = new ChartValues<int> { count }, PushOut = 15 });
+                }
+                ShowLabel = Data.Count <= LIMIT;
+            }
+        }
+
+        private void DoChartHiddenCommand(object obj)
+        {
+            ShowChart = false;
+        }
+
         private void LoadData()
         {
             list.Clear();
             list = Database.GetContext().Database.SqlQuery<ChartData>(query).ToList();
+            previousFilterValue = 0;
             if (list.Count > LIMIT)
             {
                 MaxFilter = list[LIMIT - 2].Value;
@@ -163,33 +228,6 @@ namespace LTEDatabase.ViewModel
             }
             DoFilterCommand(null);
             ShowChart = true;
-        }
-
-        private void DoFilterCommand(object obj)
-        {
-            Data.Clear();
-            int count = 0;
-            foreach(ChartData x in list)
-            {
-                if (x.Value >= FilterValue)
-                {
-                    Data.Add(new PieSeries() { Title = x.Name, Values = new ChartValues<int> { x.Value } });
-                }
-                else
-                {
-                    count += x.Value;
-                }
-            }
-            if (count > 0)
-            {
-                Data.Add(new PieSeries() { Title = "решта", Values = new ChartValues<int> { count }, PushOut = 15 }); 
-            }
-            ShowLabel = Data.Count <= LIMIT; 
-        }
-
-        private void DoChartHiddenCommand(object obj)
-        {
-            ShowChart = false;
         }
     }
 
