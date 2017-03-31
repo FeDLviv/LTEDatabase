6296e05ea8b6b3c2829c287d792189d94f52bc8c
FeD Lviv <FeD Lviv>
2017-03-31 21:57:44 +0300

    Some change(Chartwindow, ChartViewModel, Chart) and optimization Slider

--- LTEDatabase/ViewModel/ChartViewModel.cs
+++ LTEDatabase/ViewModel/ChartViewModel.cs
@@ -15,25 +15,25 @@ namespace LTEDatabase.ViewModel
     class ChartViewModel : BaseViewModel
     {
         private const int LIMIT = 8;
-        private const string QUERY_MOTOR = "SELECT motors_lte.series AS Name, COUNT(motors_lte.idMotorsLTE) AS Value FROM motors_lte JOIN objects USING(idObject) WHERE objects.type NOT IN ('склад', 'ТЕЦ', 'ТЦ') GROUP BY Name ORDER BY Value DESC;";
-        private const string QUERY_METER = "SELECT meters.type AS Name, COUNT(meters_lte.idMLTE) AS Value FROM meters RIGHT JOIN meters_lte USING (idMeter) JOIN objects USING(idObject) WHERE objects.type NOT IN ('склад', 'ТЕЦ', 'ТЦ') GROUP BY Name ORDER BY Value DESC;";
-        private const string QUERY_TC = "SELECT CONCAT_WS(' ', tc.type, tc.coefficient) AS Name, COUNT(tc_lte.idTCLTE) AS Value FROM tc RIGHT JOIN tc_lte USING (idTC) JOIN objects USING(idObject) WHERE objects.type NOT IN ('склад', 'ТЕЦ', 'ТЦ') GROUP BY Name ORDER BY Value DESC;";
-        private const string QUERY_CONTRACT = "SELECT IFNULL(organization, 'без договору') AS Name, COUNT(*) AS Value FROM objects WHERE type NOT IN ('склад', 'ТЕЦ', 'ТЦ') GROUP BY Name ORDER BY organization IS NULL, Value DESC;";
-        private const string QUERY_SUBABONENT = "SELECT subabonents.name AS Name, COUNT(subabonents_lte.idSubabonentsLTE) AS Value FROM subabonents RIGHT JOIN subabonents_lte USING(idSubabonents) GROUP BY Name ORDER BY Value DESC;";
-        private const string QUERY_WETPUMP = "SELECT motors_lte.series AS Name, COUNT(motors_lte.idMotorsLTE) AS Value FROM motors_lte JOIN objects USING(idObject) WHERE objects.type NOT IN ('склад', 'ТЕЦ', 'ТЦ') AND motors_lte.type REGEXP '^DAB VA +|^DAB A +|^IMPPUMPS GHN +|^LFP LESZNO [0-9]{2}P+|^Lowara EV +|^Lowara TLC +|^Lowara TCR +|^Lowara TC +|^NOCCHI R2C +|Sprut GPD +|^Viessmann +|^Grundfos MAGNA +|^Grundfos UP +|^Grundfos UPBASIC +|^Grundfos UPS +|^Grundfos UPER +|^Wilo RS +|^Wilo Star+|^Wilo TOP-+' GROUP BY Name ORDER BY Value DESC";
+        private const string QUERY_OBJECTCONTRACT = "SELECT IFNULL(organization, 'без договору') AS Name, COUNT(*) AS Value FROM objects WHERE type NOT IN ('склад', 'ТЕЦ', 'ТЦ') GROUP BY Name ORDER BY organization IS NULL, Value DESC;";
         private const string QUERY_OBJECTREGION = "SELECT region AS Name, COUNT(*) AS Value FROM objects WHERE type NOT IN ('склад', 'ТЕЦ', 'ТЦ') GROUP BY Name ORDER BY Value DESC;";
         private const string QUERY_OBJECTCATEGORY = "SELECT	IFNULL(category, 'невідомо') AS Name, COUNT(*) AS Value FROM objects WHERE type NOT IN ('склад', 'ТЕЦ', 'ТЦ') GROUP BY Name ORDER BY Value DESC;";
         private const string QUERY_OBJECTTYPE = "SELECT	type AS Name, COUNT(*) AS Value FROM objects WHERE type NOT IN ('склад', 'ТЕЦ', 'ТЦ') GROUP BY Name ORDER BY Value DESC";
-        private const string TITLE_MOTOR = "Двигуни на об'єктах ЛМКП \"Львівтеплоенерго\"";
-        private const string TITLE_METER = "Лічильники на об'єктах ЛМКП \"Львівтеплоенерго\"";
-        private const string TITLE_TC = "Трансформатори струму на об'єктах ЛМКП \"Львівтеплоенерго\"";
-        private const string TITLE_CONTRACT = "Договори на електропостачання об'єктів ЛМКП \"Львівтеплоенерго\"";
-        private const string TITLE_SUBABONENT = "Субабоненти на об'єктах ЛМКП \"Львівтеплоенерго\"";
-        private const string TITLE_WETPUMP = "Двигуни з мокрим ротором на об'єктах ЛМКП \"Львівтеплоенерго\"";
+        private const string QUERY_SUBABONENT = "SELECT subabonents.name AS Name, COUNT(subabonents_lte.idSubabonentsLTE) AS Value FROM subabonents RIGHT JOIN subabonents_lte USING(idSubabonents) GROUP BY Name ORDER BY Value DESC;";
+        private const string QUERY_MOTOR = "SELECT motors_lte.series AS Name, COUNT(motors_lte.idMotorsLTE) AS Value FROM motors_lte JOIN objects USING(idObject) WHERE objects.type NOT IN ('склад', 'ТЕЦ', 'ТЦ') GROUP BY Name ORDER BY Value DESC;";
+        private const string QUERY_WETPUMP = "SELECT motors_lte.series AS Name, COUNT(motors_lte.idMotorsLTE) AS Value FROM motors_lte JOIN objects USING(idObject) WHERE objects.type NOT IN ('склад', 'ТЕЦ', 'ТЦ') AND motors_lte.type REGEXP '^DAB VA +|^DAB A +|^IMPPUMPS GHN +|^LFP LESZNO [0-9]{2}P+|^Lowara EV +|^Lowara TLC +|^Lowara TCR +|^Lowara TC +|^NOCCHI R2C +|Sprut GPD +|^Viessmann +|^Grundfos MAGNA +|^Grundfos UP +|^Grundfos UPBASIC +|^Grundfos UPS +|^Grundfos UPER +|^Wilo RS +|^Wilo Star+|^Wilo TOP-+' GROUP BY Name ORDER BY Value DESC";
+        private const string QUERY_METER = "SELECT meters.type AS Name, COUNT(meters_lte.idMLTE) AS Value FROM meters RIGHT JOIN meters_lte USING (idMeter) JOIN objects USING(idObject) WHERE objects.type NOT IN ('склад', 'ТЕЦ', 'ТЦ') GROUP BY Name ORDER BY Value DESC;";
+        private const string QUERY_TC = "SELECT CONCAT_WS(' ', tc.type, tc.coefficient) AS Name, COUNT(tc_lte.idTCLTE) AS Value FROM tc RIGHT JOIN tc_lte USING (idTC) JOIN objects USING(idObject) WHERE objects.type NOT IN ('склад', 'ТЕЦ', 'ТЦ') GROUP BY Name ORDER BY Value DESC;";
+        private const string TITLE_OBJECTCONTRACT = "Об'єкти ЛМКП \"Львівтеплоенерго\" розділені по договорах";
         private const string TITLE_OBJECTREGION = "Об'єкти ЛМКП \"Львівтеплоенерго\" розділені по районах";
         private const string TITLE_OBJECTCATEGORY = "Об'єкти ЛМКП \"Львівтеплоенерго\" розділені по категоріях надійності електропостачання";
         private const string TITLE_OBJECTTYPE = "Об'єкти ЛМКП \"Львівтеплоенерго\" розділені по типах";
-
+        private const string TITLE_SUBABONENT = "Субабоненти на об'єктах ЛМКП \"Львівтеплоенерго\"";
+        private const string TITLE_MOTOR = "Двигуни на об'єктах ЛМКП \"Львівтеплоенерго\"";
+        private const string TITLE_WETPUMP = "Двигуни з мокрим ротором на об'єктах ЛМКП \"Львівтеплоенерго\"";
+        private const string TITLE_METER = "Лічильники на об'єктах ЛМКП \"Львівтеплоенерго\"";
+        private const string TITLE_TC = "Трансформатори струму на об'єктах ЛМКП \"Львівтеплоенерго\"";
+        
         private string query;
         private List<ChartData> list = new List<ChartData>();
         private int previousFilterValue;
@@ -135,16 +135,16 @@ namespace LTEDatabase.ViewModel
                 }
             }
         }
-        
-        public ICommand PieChartMotorCommand { get; set; }
-        public ICommand PieChartMeterCommand { get; set; }
-        public ICommand PieChartTCCommand { get; set; }
-        public ICommand PieChartContractCommand { get; set; }
-        public ICommand PieChartSubabonentCommand { get; set; }
-        public ICommand PieChartWetPumpCommand { get; set; }
+
+        public ICommand PieChartObjectContractCommand { get; set; }
         public ICommand PieChartObjectRegionCommand { get; set; }
         public ICommand PieChartObjectCategoryCommand { get; set; }
         public ICommand PieChartObjectTypeCommand { get; set; }
+        public ICommand PieChartSubabonentCommand { get; set; }
+        public ICommand PieChartMotorCommand { get; set; }
+        public ICommand PieChartWetPumpCommand { get; set; }
+        public ICommand PieChartMeterCommand { get; set; }
+        public ICommand PieChartTCCommand { get; set; }           
         public ICommand FilterCommand { get; set; }
         public ICommand ChartHiddenCommand { get; set; }
 
@@ -158,59 +158,24 @@ namespace LTEDatabase.ViewModel
             //ПРОКРУТКА КРУГОВОЇ ДІАГРАМИ
             
             //ГІСТОГРАММА (Basic Column)
-            
-            PieChartMotorCommand = new BaseCommand(DoPieChartMotorCommand);
-            PieChartMeterCommand = new BaseCommand(DoPieChartMeterCommand);
-            PieChartTCCommand = new BaseCommand(DoPieChartTCCommand);
-            PieChartContractCommand = new BaseCommand(DoPieChartContractCommand);
-            PieChartSubabonentCommand = new BaseCommand(DoPieChartSubabonentCommand);
-            PieChartWetPumpCommand = new BaseCommand(DoPieChartWetPumpCommand);
+
+            PieChartObjectContractCommand = new BaseCommand(DoPieChartObjectContractCommand);
             PieChartObjectRegionCommand = new BaseCommand(DoPieChartObjectRegionCommand);
             PieChartObjectCategoryCommand = new BaseCommand(DoPieChartObjectCategoryCommand);
             PieChartObjectTypeCommand = new BaseCommand(DoPieChartObjectTypeCommand);
+            PieChartSubabonentCommand = new BaseCommand(DoPieChartSubabonentCommand);
+            PieChartMotorCommand = new BaseCommand(DoPieChartMotorCommand);
+            PieChartWetPumpCommand = new BaseCommand(DoPieChartWetPumpCommand);
+            PieChartMeterCommand = new BaseCommand(DoPieChartMeterCommand);
+            PieChartTCCommand = new BaseCommand(DoPieChartTCCommand);
             FilterCommand = new BaseCommand(DoFilterCommand);
             ChartHiddenCommand = new BaseCommand(DoChartHiddenCommand);
         }
 
-        private void DoPieChartMotorCommand(object obj)
+        private void DoPieChartObjectContractCommand(object obj)
         {
-            TitleChart = TITLE_MOTOR;
-            query = QUERY_MOTOR;
-            LoadData();
-        }
-
-        private void DoPieChartMeterCommand(object obj)
-        {
-            TitleChart = TITLE_METER;
-            query = QUERY_METER;
-            LoadData();
-        }
-
-        private void DoPieChartTCCommand(object obj)
-        {
-            TitleChart = TITLE_TC;
-            query = QUERY_TC;
-            LoadData();
-        }
-
-        private void DoPieChartContractCommand(object obj)
-        {
-            TitleChart = TITLE_CONTRACT;
-            query = QUERY_CONTRACT;
-            LoadData();
-        }
-
-        private void DoPieChartSubabonentCommand(object obj)
-        {
-            TitleChart = TITLE_SUBABONENT;
-            query = QUERY_SUBABONENT;
-            LoadData();
-        }
-
-        private void DoPieChartWetPumpCommand(object obj)
-        {
-            TitleChart = TITLE_WETPUMP;
-            query = QUERY_WETPUMP;
+            TitleChart = TITLE_OBJECTCONTRACT;
+            query = QUERY_OBJECTCONTRACT;
             LoadData();
         }
 
@@ -235,6 +200,41 @@ namespace LTEDatabase.ViewModel
             LoadData();
         }
 
+        private void DoPieChartSubabonentCommand(object obj)
+        {
+            TitleChart = TITLE_SUBABONENT;
+            query = QUERY_SUBABONENT;
+            LoadData();
+        }
+
+        private void DoPieChartMotorCommand(object obj)
+        {
+            TitleChart = TITLE_MOTOR;
+            query = QUERY_MOTOR;
+            LoadData();
+        }
+
+        private void DoPieChartWetPumpCommand(object obj)
+        {
+            TitleChart = TITLE_WETPUMP;
+            query = QUERY_WETPUMP;
+            LoadData();
+        }
+
+        private void DoPieChartMeterCommand(object obj)
+        {
+            TitleChart = TITLE_METER;
+            query = QUERY_METER;
+            LoadData();
+        }
+
+        private void DoPieChartTCCommand(object obj)
+        {
+            TitleChart = TITLE_TC;
+            query = QUERY_TC;
+            LoadData();
+        }
+       
         private void DoFilterCommand(object obj)
         {
             if (FilterValue != previousFilterValue)
@@ -273,7 +273,11 @@ namespace LTEDatabase.ViewModel
             previousFilterValue = 0;
             if (list.Count > LIMIT)
             {
-                MaxFilter = list[LIMIT - 2].Value;
+                MaxFilter = list[LIMIT - 1].Value + 1;
+                if (MaxFilter < 5 && list[0].Value > 5)
+                {
+                    MaxFilter = 5;
+                }
                 FilterValue = MaxFilter;
             }
             else
